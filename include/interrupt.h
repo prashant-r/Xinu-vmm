3d5439088a414bb11a0558494da2cb8079683595
prashant-r <prashantr186@yahoo.com>
2016-04-25 02:38:04 -0400

    finally correct values

--- include/interrupt.h
+++
@@ -1,5 +0,0 @@
-/* interrupt.h */
-
-extern	uint16	girmask;	/* mask of interrupts being serviced	*/
-				/* a bit of 1 corresponds to an		*/
-				/* interrupt that is allowed.		*/
--- paging/paging_tests.c
+++
@@ -1,9 +0,0 @@
-#include <xinu.h>
-
-void all_paging_tests()
-{
-
-	vcreate_tests();
-
-
-}
